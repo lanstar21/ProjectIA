e819526a6c1e528e786532e3d9b4dd59c81c856c
lanstar21 <lanstar.804@gmail.com>
2019-01-27 19:01:37 -0500

    started hello world

---
+++ ProjectIA/Controllers/HelloController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectIA.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return Content("<h1>Hello World</h1>", "text/html");
+        }
+
+        public IActionResult Goodbye()
+        {
+            return Content("Goodbye");
+        }
+    }
+}
---
+++ ProjectIA/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjectIA.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
