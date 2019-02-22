ccb4dcb27750515fc4b3dc7d96b7fc85ed260e80
lanstar21 <lanstar.804@gmail.com>
2019-02-22 18:53:03 -0500

    got the user signup page partially working

--- ProjectIA/Controllers/HomeController.cs
+++ ProjectIA/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace ProjectIA.Controllers
         {
             return View();
         }
+        
+        public IActionResult Signup()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
